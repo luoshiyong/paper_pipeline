9f4a99929493f68380ab16ca62aede8057c7392f
Luo shiyong <15811532979@163.com>
2022-09-10 17:23:29 +0800

    Create axis_transformer.py

---
+++ net/axis_transformer.py
@@ -0,0 +1,225 @@
+import copy
+import logging
+import math
+
+from os.path import join as pjoin
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNorm
+from torch.nn.modules.utils import _pair
+from scipy import ndimage
+import torch.nn.functional as F
+from torchvision.ops.boxes import box_area
+def swish(x):
+    return x * torch.sigmoid(x)
+
+
+ACT2FN = {"gelu": torch.nn.functional.gelu, "relu": torch.nn.functional.relu, "swish": swish}
+
+class Attention(nn.Module):
+    def __init__(self, hidden_size,num_heads,attention_dropout_rate, vis=False):
+        super(Attention, self).__init__()
+        self.vis = vis
+        self.num_attention_heads = num_heads
+        self.attention_head_size = int(hidden_size / self.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = Linear(hidden_size, self.all_head_size)
+        self.key = Linear(hidden_size, self.all_head_size)
+        self.value = Linear(hidden_size, self.all_head_size)
+
+        self.out = Linear(hidden_size, hidden_size)
+        self.attn_dropout = Dropout(attention_dropout_rate)
+        self.proj_dropout = Dropout(attention_dropout_rate)
+
+        self.softmax = Softmax(dim=-1)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states):
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        mixed_value_layer = self.value(hidden_states)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+        key_layer = self.transpose_for_scores(mixed_key_layer)
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_probs = self.softmax(attention_scores)
+        weights = attention_probs if self.vis else None
+        attention_probs = self.attn_dropout(attention_probs)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+        attention_output = self.out(context_layer)
+        attention_output = self.proj_dropout(attention_output)
+        return attention_output, weights
+class qkv_transform(nn.Conv1d):
+    """Conv1d for qkv_transform"""
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+class AxialAttention(nn.Module):
+    def __init__(self, in_planes, out_planes, groups=8, kernel_size=56,
+                 stride=1, bias=False, width=False):
+        assert (in_planes % groups == 0) and (out_planes % groups == 0)
+        super(AxialAttention, self).__init__()
+        self.in_planes = in_planes
+        self.out_planes = out_planes
+        self.groups = groups
+        self.group_planes = out_planes // groups
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.bias = bias
+        self.width = width
+
+        # Multi-head self attention
+        self.qkv_transform = qkv_transform(in_planes, out_planes * 2, kernel_size=1, stride=1,
+                                           padding=0, bias=False)
+        self.bn_qkv = nn.BatchNorm1d(out_planes * 2)
+        self.bn_similarity = nn.BatchNorm2d(groups * 3)
+        #self.bn_qk = nn.BatchNorm2d(groups)
+        #self.bn_qr = nn.BatchNorm2d(groups)
+        #self.bn_kr = nn.BatchNorm2d(groups)
+        self.bn_output = nn.BatchNorm1d(out_planes * 2)
+
+        # Position embedding
+        self.relative = nn.Parameter(torch.randn(self.group_planes * 2, kernel_size * 2 - 1), requires_grad=True)
+        query_index = torch.arange(kernel_size).unsqueeze(0)
+        key_index = torch.arange(kernel_size).unsqueeze(1)
+        relative_index = key_index - query_index + kernel_size - 1
+        self.register_buffer('flatten_index', relative_index.view(-1))
+        if stride > 1:
+            self.pooling = nn.AvgPool2d(stride, stride=stride)
+
+        self.reset_parameters()
+
+    def forward(self, x):
+        if self.width:
+            x = x.permute(0, 2, 1, 3)
+        else:
+            x = x.permute(0, 3, 1, 2)  # N, W, C, H
+        N, W, C, H = x.shape
+        x = x.contiguous().view(N * W, C, H)
+
+        # Transformations
+        qkv = self.bn_qkv(self.qkv_transform(x))
+        q, k, v = torch.split(qkv.reshape(N * W, self.groups, self.group_planes * 2, H), [self.group_planes // 2, self.group_planes // 2, self.group_planes], dim=2)
+
+        # Calculate position embedding
+        all_embeddings = torch.index_select(self.relative, 1, self.flatten_index).view(self.group_planes * 2, self.kernel_size, self.kernel_size)
+        q_embedding, k_embedding, v_embedding = torch.split(all_embeddings, [self.group_planes // 2, self.group_planes // 2, self.group_planes], dim=0)
+        qr = torch.einsum('bgci,cij->bgij', q, q_embedding)
+        kr = torch.einsum('bgci,cij->bgij', k, k_embedding).transpose(2, 3)
+        qk = torch.einsum('bgci, bgcj->bgij', q, k)
+        stacked_similarity = torch.cat([qk, qr, kr], dim=1)
+        stacked_similarity = self.bn_similarity(stacked_similarity).view(N * W, 3, self.groups, H, H).sum(dim=1)
+        #stacked_similarity = self.bn_qr(qr) + self.bn_kr(kr) + self.bn_qk(qk)
+        # (N, groups, H, H, W)
+        similarity = F.softmax(stacked_similarity, dim=3)
+        sv = torch.einsum('bgij,bgcj->bgci', similarity, v)
+        sve = torch.einsum('bgij,cij->bgci', similarity, v_embedding)
+        stacked_output = torch.cat([sv, sve], dim=-1).view(N * W, self.out_planes * 2, H)
+        output = self.bn_output(stacked_output).view(N, W, self.out_planes, 2, H).sum(dim=-2)
+
+        if self.width:
+            output = output.permute(0, 2, 1, 3)
+        else:
+            output = output.permute(0, 2, 3, 1)
+
+        if self.stride > 1:
+            output = self.pooling(output)
+
+        return output
+
+    def reset_parameters(self):
+        self.qkv_transform.weight.data.normal_(0, math.sqrt(1. / self.in_planes))
+        #nn.init.uniform_(self.relative, -0.1, 0.1)
+        nn.init.normal_(self.relative, 0., math.sqrt(1. / self.group_planes))
+
+class Mlp(nn.Module):
+    def __init__(self, hidden_size,mlp_dim,dropout_rate):
+        super(Mlp, self).__init__()
+        self.fc1 = Linear(hidden_size, mlp_dim)
+        self.fc2 = Linear(mlp_dim, hidden_size)
+        self.act_fn = ACT2FN["gelu"]
+        self.dropout = Dropout(dropout_rate)
+
+        self._init_weights()
+
+    def _init_weights(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.normal_(self.fc1.bias, std=1e-6)
+        nn.init.normal_(self.fc2.bias, std=1e-6)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act_fn(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+from tmpp import AxialBlock
+class Block(nn.Module):
+    def __init__(self, hidden_size,mlp_dim,num_heads,dropout_rate=0.2,attention_dropout_rate=0.2,attention_type='atten',vis=False):
+        super(Block, self).__init__()
+        self.hidden_size = hidden_size
+        self.attention_norm = LayerNorm(hidden_size, eps=1e-6)
+        self.ffn_norm = LayerNorm(hidden_size, eps=1e-6)
+        self.ffn = Mlp(hidden_size,mlp_dim,dropout_rate)
+        if attention_type=='atten':
+            self.attn = Attention(hidden_size,num_heads,attention_dropout_rate,vis)
+        elif attention_type=="axis_atten":
+            self.attn = AxialBlock(inplanes=512, planes=512,groups=8,base_width=64,dilation=1,kernel_size=14)
+        else:
+            self.attn = Attention(hidden_size, num_heads, attention_dropout_rate, vis)
+
+    def forward(self, x):
+        h = x
+        x = self.attention_norm(x)
+        x, weights = self.attn(x)
+        x = x + h
+
+        h = x
+        x = self.ffn_norm(x)
+        x = self.ffn(x)
+        x = x + h
+        return x, weights
+class Encoder(nn.Module):
+    def __init__(self, mlp_dim,num_heads,hidden_size=256,num_layers=12, vis=False):
+        super(Encoder, self).__init__()
+        self.vis = vis
+        self.layer = nn.ModuleList()
+        self.encoder_norm = LayerNorm(hidden_size, eps=1e-6)
+        for _ in range(num_layers):
+            layer = Block(hidden_size,mlp_dim,num_heads,vis)
+            self.layer.append(copy.deepcopy(layer))
+
+    def forward(self, hidden_states):
+        attn_weights = []
+        for layer_block in self.layer:
+            hidden_states, weights = layer_block(hidden_states)
+            if self.vis:
+                attn_weights.append(weights)
+        encoded = self.encoder_norm(hidden_states)
+        return encoded, attn_weights
+def get_transformer():
+    encoder_st = Encoder(mlp_dim=441,num_heads=9,hidden_size=441)
+    return encoder_st
+
+
+input = torch.randn(2,512,441)  # 512个441向量
+model = get_transformer()
+out,_ = model(input)
+print(out.shape)
